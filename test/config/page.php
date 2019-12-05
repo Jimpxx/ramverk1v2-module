1496c482429b240e6b05670f207814b042920d28
Jimmy Andersson <jimmyandersson89@gmail.com>
2019-12-05 14:49:09 +0000

    Made the tests go through on the module

--- test/config/page.php
+++ test/config/page.php
@@ -10,87 +10,12 @@ return [
     // all the other views are rendered.
     "layout" => [
         "region" => "layout",
-        "template" => "anax/v2/layout/dbwebb_se",
+        "template" => "test/layout",
         "data" => [
-            "baseTitle" => " | ramverk1",
-            "bodyClass" => null,
-            "favicon" => "favicon.ico",
-            "htmlClass" => null,
             "lang" => "sv",
-            "stylesheets" => [
-                "css/dbwebb-se.min.css",
-            ],
-            "javascripts" => [
-                "js/responsive-menu.js",
-            ],
         ],
     ],
 
     // These views are always loaded into the collection of views.
-    "views" => [
-        [
-            "region" => "header-col-1",
-            "template" => "anax/v2/header/site_logo",
-            "data" => [
-                "class" => "large",
-                "siteLogo"      => "image/theme/leaf_256x256.png",
-                "siteLogoAlt"   => "Löv",
-            ],
-        ],
-        [
-            "region" => "header-col-1",
-            "template" => "anax/v2/header/site_logo_text",
-            "data" => [
-                "homeLink"      => "",
-                "siteLogoText"  => "ramverk1",
-                "siteLogoTextIcon" => "image/theme/leaf_40x40.png",
-                "siteLogoTextIconAlt" => "Löv-bild",
-            ],
-        ],
-        [
-            "region" => "header-col-2",
-            "template" => "anax/v2/navbar/navbar_submenus",
-            "data" => [
-                "navbarConfig" => require __DIR__ . "/navbar/header.php",
-            ],
-        ],
-        [
-            "region" => "header-col-3",
-            "template" => "anax/v2/navbar/responsive_submenus",
-            "data" => [
-                "navbarConfig" => require __DIR__ . "/navbar/responsive.php",
-            ],
-        ],
-        [
-            "region" => "footer",
-            "template" => "anax/v2/columns/multiple_columns",
-            "data" => [
-                "class"  => "footer-column",
-                "columns" => [
-                    [
-                        "template" => "anax/v2/block/default",
-                        "contentRoute" => "block/footer-col-1",
-                    ],
-                    [
-                        "template" => "anax/v2/block/default",
-                        "contentRoute" => "block/footer-col-2",
-                    ],
-                    [
-                        "template" => "anax/v2/block/default",
-                        "contentRoute" => "block/footer-col-3",
-                    ]
-                ]
-            ],
-            "sort" => 1
-        ],
-        [
-            "region" => "footer",
-            "template" => "anax/v2/block/default",
-            "data" => [
-                "class"  => "site-footer",
-                "contentRoute" => "block/footer",
-            ],
-            "sort" => 2
-        ],
-    ],
+    "views" => [],
 ];
