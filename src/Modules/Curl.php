f768861cbb829279866fdf495de496918db0d0cf
Jimmy Andersson <jimmyandersson89@gmail.com>
2019-12-08 21:55:22 +0000

    Cleaned up some unwanted comments

--- src/Modules/Curl.php
+++ src/Modules/Curl.php
@@ -124,46 +124,4 @@ class Curl
         // curl_close($ch);
         // return json_decode($info);
     }
-
-    // public function getUsersThroughMultiCurl(array $userIds) : array
-    // {
-    //     $url = "https://rem.dbwebb.se/api/users";
-
-    //     $options = [
-    //         CURLOPT_RETURNTRANSFER => true,
-    //     ];
-
-    //     // Add all curl handlers and remember them
-    //     // Initiate the multi curl handler
-    //     $mh = curl_multi_init();
-    //     $chAll = [];
-    //     foreach ($userIds as $id) {
-    //         $ch = curl_init("$url/$id");
-    //         curl_setopt_array($ch, $options);
-    //         curl_multi_add_handle($mh, $ch);
-    //         $chAll[] = $ch;
-    //     }
-
-    //     // Execute all queries simultaneously,
-    //     // and continue when all are complete
-    //     $running = null;
-    //     do {
-    //         curl_multi_exec($mh, $running);
-    //     } while ($running);
-
-    //     // Close the handles
-    //     foreach ($chAll as $ch) {
-    //         curl_multi_remove_handle($mh, $ch);
-    //     }
-    //     curl_multi_close($mh);
-
-    //     // All of our requests are done, we can now access the results
-    //     $response = [];
-    //     foreach ($chAll as $ch) {
-    //         $data = curl_multi_getcontent($ch);
-    //         $response[] = json_decode($data, true);
-    //     }
-
-    //     return $response;
-    // }
 }
